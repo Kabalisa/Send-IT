6c791c53cb7e9463fe9c938f89e5b9e9f8809414
kabalisa innocent fiston <ikabalisa20@gmail.com>
2018-11-01 08:52:48 -0400

    creating the header

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
-var a = null;
+function yello(){
+	document.getElementById("1").style.color = '#FFCC00';
+}
+
+function yello1(){
+	document.getElementById("2").style.color = '#FFCC00';
+}
+
+function yello2(){
+	document.getElementById("3").style.color = '#FFCC00';
+}
+
+function yello3(){
+	document.getElementById("4").style.color = '#FFCC00';
+}
+
+function yello4(){
+	document.getElementById("5").style.color = '#FFCC00';
+}
+
+function whit(){
+	document.getElementById("1").style.color = 'white';
+	document.getElementById("2").style.color = 'white';
+	document.getElementById("3").style.color = 'white';
+	document.getElementById("4").style.color = 'white';
+	document.getElementById("5").style.color = 'white';
+}
