132af47dfe5d700ceee773356c8a4e0ad2ff8710
Clet Mwunguzi <mwunguzicl@gmail.com>
2018-11-02 20:42:50 +0200

    order-details

---
+++ js/editable.js
@@ -0,0 +1,23 @@
+
+function ondis(){
+var x = document.getElementsByTagName('input');
+for(i=0; i<12; i++){	
+ x[i].disabled=false;
+}
+
+
+}
+
+function onenable(){
+var x = document.getElementsByTagName('input');
+for(i=0; i<12; i++){	
+ x[i].disabled=true;
+}
+
+}
+
+function onenable1(){
+
+	var cont = document.getElementsByClassName('hidee').innerHTML="  ";
+	
+}
